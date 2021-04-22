2170d02b9e39a5a16b8e1a2dea948fe2e5e7c274
ArtemSirobaba <59284335+ArtemSirobaba@users.noreply.github.com>
2021-04-22 17:53:12 +0300

    update component

--- src/components/Song/Button.tsx
+++ src/components/Song/Button.tsx
@@ -12,6 +12,7 @@ const Button = ({ sound, name }: ButtonProps) => {
   return (
     <div>
       <input
+      className='cursor-pointer m-2 p-2 '
         onClick={(event: React.MouseEvent<HTMLElement>) => play()}
         type="submit"
         value={name}
--- src/components/Song/Song.tsx
+++ src/components/Song/Song.tsx
@@ -19,24 +19,32 @@ import Button from './Button'
 
 const Song = () => {
   return (
-    <>
-      <Button name="WORK IT" sound={workit} />
-      <Button name="MAKE IT" sound={makeit} />
-      <Button name="DO IT" sound={doit} />
-      <Button name="MAKES US" sound={makesus} />
-      <Button name="HARDER" sound={harder} />
-      <Button name="BETTER" sound={better} />
-      <Button name="FASTER" sound={faster} />
-      <Button name="STRONGER" sound={stronger} />
-      <Button name="MORE THAN" sound={morethan} />
-      <Button name="HOUR" sound={hour} />
-      <Button name="OUR" sound={our} />
-      <Button name="NEVER" sound={never} />
-      <Button name="EVER" sound={ever} />
-      <Button name="AFTER" sound={after} />
-      <Button name="WORK IS" sound={workis} />
-      <Button name="OVER" sound={over} />
-    </>
+    <div className="flex justify-center items-center flex-col">
+      <div className="flex justify-between w-54">
+        <Button name="WORK IT" sound={workit} />
+        <Button name="MAKE IT" sound={makeit} />
+        <Button name="DO IT" sound={doit} />
+        <Button name="MAKES US" sound={makesus} />
+      </div>
+      <div className="flex justify-between">
+        <Button name="HARDER" sound={harder} />
+        <Button name="BETTER" sound={better} />
+        <Button name="FASTER" sound={faster} />
+        <Button name="STRONGER" sound={stronger} />
+      </div>
+      <div className="flex justify-between">
+        <Button name="MORE THAN" sound={morethan} />
+        <Button name="HOUR" sound={hour} />
+        <Button name="OUR" sound={our} />
+        <Button name="NEVER" sound={never} />
+      </div>
+      <div className="flex justify-between">
+        <Button name="EVER" sound={ever} />
+        <Button name="AFTER" sound={after} />
+        <Button name="WORK IS" sound={workis} />
+        <Button name="OVER" sound={over} />
+      </div>
+    </div>
   )
 }
 
