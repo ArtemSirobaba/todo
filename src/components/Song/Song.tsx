82aba7653619a12fc849b43d2f4ab617a6f2ecb4
ArtemSirobaba <59284335+ArtemSirobaba@users.noreply.github.com>
2021-04-23 22:08:08 +0300

    change names

--- src/components/Song/Song.tsx
+++ src/components/Song/Song.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Button from './Button'
-import nameArray from './nameArray'
 
 const Song = () => {
   const data = useStaticQuery(graphql`
@@ -9,24 +8,22 @@ const Song = () => {
       allFile(filter: { extension: { eq: "mp3" } }) {
         edges {
           node {
+            name
             publicURL
           }
         }
       }
     }
   `)
-    const newData = data.allFile.edges.sort(
-      (a: any, b: any) => a.node.publicURL - b.node.publicURL
-    )
-    console.log(newData)
+  
   return (
     <div className="flex justify-center mt-10">
       <div className="grid grid-cols-4">
-        {newData.map((file: any, index: number) => (
+        {data.allFile.edges.map((file: any, index: number) => (
           <Button
             key={file.node.publicURL}
             num={index}
-            name={nameArray[index]}
+            name={file.node.name.split('-').join(' ')}
             sound={file.node.publicURL}
           />
         ))}
