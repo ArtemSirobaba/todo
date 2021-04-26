f6eb90e02d12753b0356d281dc2a88a4e77d03d5
ArtemSirobaba <59284335+ArtemSirobaba@users.noreply.github.com>
2021-04-26 21:52:40 +0300

    update

--- src/components/Queen/Queen.tsx
+++ src/components/Queen/Queen.tsx
@@ -36,13 +36,13 @@ const Queen = () => {
     <div className="flex flex-col justify-center items-center h-[80vh]">
       <div>
         <input
-          className="p-4 m-2 rounded-xl cursor-pointer border-2 border-red-600 hover:bg-red-600 sm:text-lg hover:text-white sm:px-10 sm:p-8 outline:none"
+          className="p-4 m-2 rounded-xl cursor-pointer border-2 border-red-600 hover:bg-red-600 sm:text-lg hover:text-white sm:px-10 sm:p-8 outline-none"
           value="Beat-1 (V key)"
           type="submit"
           onClick={(event: React.MouseEvent<HTMLElement>) => beat1()}
         />
         <input
-          className="p-4 m-2 rounded-xl cursor-pointer hover:bg-blue-600 border-2 border-blue-600 sm:text-lg hover:text-white sm:px-10 sm:p-8 outline:none"
+          className="p-4 m-2 rounded-xl cursor-pointer hover:bg-blue-600 border-2 border-blue-600 sm:text-lg hover:text-white sm:px-10 sm:p-8 outline-none"
           value="Beat-2 (N key)"
           type="submit"
           onClick={(event: React.MouseEvent<HTMLElement>) => beat2()}
@@ -51,7 +51,7 @@ const Queen = () => {
         <Hotkeys keyName="N" onKeyUp={() => beat2()} />
       </div>
       <input
-        className="px-4 p-1 m-6 rounded-md cursor-pointer hover:bg-yellow-600 hover:text-white sm:px-10 sm:p-4 sm:text-lg outline:none"
+        className="m-6 px-6 py-2 rounded-md cursor-pointer hover:bg-yellow-600 hover:text-white sm:px-10 sm:p-4 sm:text-lg outline-none"
         value={!isPlay ? 'Play' : 'Stop'}
         type="submit"
         onClick={handleSound}
