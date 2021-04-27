71bf808888639749701e64f353966bee8eae2c4c
ArtemSirobaba <59284335+ArtemSirobaba@users.noreply.github.com>
2021-04-27 20:03:41 +0300

    update queen

--- src/components/Queen/Queen.tsx
+++ src/components/Queen/Queen.tsx
@@ -6,7 +6,10 @@ import Hotkeys from 'react-hot-keys'
 const Queen = () => {
   const data = useStaticQuery(graphql`
     {
-      allFile(filter: { relativeDirectory: { eq: "Queen" } }) {
+      allFile(
+        filter: { relativeDirectory: { eq: "Queen" } }
+        sort: { fields: name }
+      ) {
         edges {
           node {
             id
@@ -21,6 +24,7 @@ const Queen = () => {
   const [beat2] = useSound(data.allFile.edges[1].node.publicURL)
   const [music, { stop }] = useSound(data.allFile.edges[2].node.publicURL)
   const [isPlay, setIsPlay] = useState(false)
+  const [autoPlay, setAutoPlay] = useState(false)
 
   const handleSound = (event: React.MouseEvent<HTMLElement>) => {
     if (!isPlay) {
@@ -32,6 +36,9 @@ const Queen = () => {
     }
   }
 
+  const handleAutoPlay = () => {
+    setAutoPlay(!autoPlay) 
+  }
   return (
     <div className="flex flex-col justify-center items-center h-[80vh]">
       <div>
@@ -47,14 +54,20 @@ const Queen = () => {
           type="submit"
           onClick={(event: React.MouseEvent<HTMLElement>) => beat2()}
         />
-        <Hotkeys keyName="V" onKeyUp={() => beat1()} />
-        <Hotkeys keyName="N" onKeyUp={() => beat2()} />
+        <Hotkeys keyName="V" onKeyDown={() => beat1()} />
+        <Hotkeys keyName="N" onKeyDown={() => beat2()} />
       </div>
       <input
         className="m-6 px-6 py-2 rounded-md cursor-pointer hover:bg-yellow-600 hover:text-white sm:px-10 sm:p-4 sm:text-lg outline-none"
         value={!isPlay ? 'Play' : 'Stop'}
         type="submit"
         onClick={handleSound}
+      />
+      <input
+        className="m-1 px-6 py-2 rounded-md cursor-pointer sm:px-10 sm:p-4 sm:text-lg outline-none"
+        type="submit"
+        onClick={handleAutoPlay}
+        value={`Autoplay ${autoPlay ? '⏸' : '▶'}`}
       />
     </div>
   )
